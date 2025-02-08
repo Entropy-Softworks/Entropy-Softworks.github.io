80b1880e492fa514e56a65c7d90d43297b47fffe
Geoffrey Sherrill <geoff@entropysoftworks.com>
2025-02-08 13:17:38 -0500

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/Entropy-Softworks.github.io/',
-  plugins: [react()],
-})
